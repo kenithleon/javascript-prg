b6625bc041f1949036d84d405c255a08142b3182
Kenith Leon <162732766+kenithleon@users.noreply.github.com>
2025-10-01 20:35:55 +0530

    Find Maximum Number -6
    Find Maximum Number – JavaScript

---
+++ findmax.js
@@ -0,0 +1,11 @@
+const findMax = (arr) => {
+    return Math.max(...arr);
+}
+
+
+
+console.log(findMax([3, 5, 70, 2, 8])); // Output: 8
+console.log(findMax([-10, -3, -1, -7])); // Output: -1
+console.log(findMax[5]);
+
+
